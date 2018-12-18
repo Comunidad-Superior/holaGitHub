bcd94989c389c098a9b482f5709ebfe421a04e52
Antonio <starius@MBP-de-Antonio.home>
2018-12-18 01:35:55 +0100

    modificacionTres

--- HolaGH.java
+++ HolaGH.java
@@ -1,8 +1,7 @@
 public class HolaGH {
     public static void main(String [] args){
         System.out.println("Hola Mundo");
-        System.out.println("Hola Mundo");
-        System.out.println("Hola Mundo");
+        System.out.println("Hola MundoLLL");
     }
 }
 
